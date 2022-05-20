a2d20cb3903baa4bdce71c89b34a5bbe37cc20ac
Phil Miller <137389+idrop@users.noreply.github.com>
2022-05-20 17:08:27 +0100

    Revert "Revert "[KBV-399] Refactor to remove address specific functionality"" (#13)

---
+++ src/main/java/uk/gov/di/ipv/cri/common/library/domain/sharedclaims/Name.java
@@ -0,0 +1,34 @@
+package uk.gov.di.ipv.cri.common.library.domain.sharedclaims;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Name {
+    private List<NamePart> nameParts;
+    private LocalDate validFrom;
+    private LocalDate validUntil;
+
+    public List<NamePart> getNameParts() {
+        return nameParts;
+    }
+
+    public void setNameParts(List<NamePart> nameParts) {
+        this.nameParts = nameParts;
+    }
+
+    public LocalDate getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(LocalDate validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public LocalDate getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(LocalDate validUntil) {
+        this.validUntil = validUntil;
+    }
+}
