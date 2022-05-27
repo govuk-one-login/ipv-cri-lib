fbd88ff28e7f1ab0afd4dae5f279820bd6f15a99
Stuart Foulds-Duncan <stuart.foulds-duncan@hippodigital.co.uk>
2022-05-27 16:34:52 +0100

    Corrected comments

--- src/main/java/uk/gov/di/ipv/cri/common/library/domain/AuditEventTypes.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/domain/AuditEventTypes.java
@@ -6,6 +6,6 @@ public enum AuditEventTypes {
     IPV_ADDRESS_CRI_VC_ISSUED, // When the final VC is created in the issue credential lambda
 
     IPV_KBV_CRI_START, // Before a session is written to the Session table
-    IPV_KBV_CRI_REQUEST_SENT, // When an address is added in the PUT /address API call
+    IPV_KBV_CRI_REQUEST_SENT, // When KBV answers are submitted
     IPV_KBV_CRI_VC_ISSUED // When the final VC is created in the issue credential lambda
 }
