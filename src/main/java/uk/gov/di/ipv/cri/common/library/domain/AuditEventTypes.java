9856c79d1cc49ae3621c9d07488f13fc95137eea
Stuart Foulds-Duncan <stuart.foulds-duncan@digital.cabinet-office.gov.uk>
2022-05-30 11:51:37 +0100

    [KBV-106] Add new event types (#20)
    * Add new event types
    
    * Corrected comments

--- src/main/java/uk/gov/di/ipv/cri/common/library/domain/AuditEventTypes.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/domain/AuditEventTypes.java
@@ -3,5 +3,9 @@ package uk.gov.di.ipv.cri.common.library.domain;
 public enum AuditEventTypes {
     IPV_ADDRESS_CRI_START, // Before a session is written to the Session table
     IPV_ADDRESS_CRI_REQUEST_SENT, // When an address is added in the PUT /address API call
-    IPV_ADDRESS_CRI_VC_ISSUED // When the final VC is created in the issue credential lambda
+    IPV_ADDRESS_CRI_VC_ISSUED, // When the final VC is created in the issue credential lambda
+
+    IPV_KBV_CRI_START, // Before a session is written to the Session table
+    IPV_KBV_CRI_REQUEST_SENT, // When KBV answers are submitted
+    IPV_KBV_CRI_VC_ISSUED // When the final VC is created in the issue credential lambda
 }
