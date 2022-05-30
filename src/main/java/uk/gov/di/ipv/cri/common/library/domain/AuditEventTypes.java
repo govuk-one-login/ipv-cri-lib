6c0b10114e25d842df676c5841656a9f6e795ead
Aman Mohammed <95495586+techaman-m@users.noreply.github.com>
2022-05-30 12:48:43 +0100

    KBV-432 - Integrate audit event feature for KBV CRI (#21)

--- src/main/java/uk/gov/di/ipv/cri/common/library/domain/AuditEventTypes.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/domain/AuditEventTypes.java
@@ -1,11 +1,14 @@
 package uk.gov.di.ipv.cri.common.library.domain;
 
 public enum AuditEventTypes {
-    IPV_ADDRESS_CRI_START, // Before a session is written to the Session table
+    IPV_ADDRESS_CRI_START, // Before a session is written to the Session table for Address CRI
     IPV_ADDRESS_CRI_REQUEST_SENT, // When an address is added in the PUT /address API call
     IPV_ADDRESS_CRI_VC_ISSUED, // When the final VC is created in the issue credential lambda
 
-    IPV_KBV_CRI_START, // Before a session is written to the Session table
-    IPV_KBV_CRI_REQUEST_SENT, // When KBV answers are submitted
-    IPV_KBV_CRI_VC_ISSUED // When the final VC is created in the issue credential lambda
+    IPV_KBV_CRI_START, // Before a session is written to the Session table for KBV CRI
+    IPV_KBV_CRI_REQUEST_SENT, // First request sent for the KBV Experian SOAP API
+    IPV_KBV_CRI_VC_ISSUED, // When the final VC is created in the issue credential lambda
+    IPV_KBV_CRI_THIRD_PARTY_REQUEST_ENDED, // last request sent for the KBV API
+
+    IPV_FRAUD_CRI_START // Before a session is written to the Session table for Fraud CRI
 }
