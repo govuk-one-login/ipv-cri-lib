037a9785e86f519f1ed327b4314367bad5cc0f11
tal-nagra <tal.nagra@digital.cabinet-office.gov.uk>
2022-06-09 17:03:47 +0100

    EventProbe - add temporary detailed exception logging

--- src/main/java/uk/gov/di/ipv/cri/common/library/util/EventProbe.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/util/EventProbe.java
@@ -7,6 +7,9 @@ import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
 import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
 import software.amazon.lambda.powertools.metrics.MetricsUtils;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Map;
 import java.util.Objects;
 
@@ -17,12 +20,32 @@ public class EventProbe {
 
     public EventProbe log(Level level, Exception e) {
         LOGGER.log(level, e);
-        if (level == Level.ERROR && Objects.nonNull(e.getCause())) {
-            LOGGER.log(level, e.getCause());
-            if (Objects.nonNull(e.getCause().getCause())) {
-                LOGGER.log(level, e.getCause().getCause());
+        if (level == Level.ERROR) {
+            if (Objects.nonNull(e.getCause())) {
+                LOGGER.log(level, e.getCause());
+                if (Objects.nonNull(e.getCause().getCause())) {
+                    LOGGER.log(level, e.getCause().getCause());
+
+                    LOGGER.error(e.getCause().getCause().getMessage(), e.getCause().getCause());
+                    LOGGER.error(e.getCause().getCause().toString());
+                }
+                LOGGER.error(e.getCause().getMessage(), e.getCause());
+                LOGGER.error(e.getCause().toString());
+            }
+
+            LOGGER.error(e.getMessage(), e);
+            LOGGER.error(e.toString());
+
+            try (StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw)) {
+                e.printStackTrace(pw);
+                String stacktrace = sw.toString();
+                LOGGER.error(stacktrace);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
             }
         }
+
         return this;
     }
 
