21df8f6e1fcacd3481dd351b50f42019def9eda3
Stuart Foulds-Duncan <stuart.foulds-duncan@hippodigital.co.uk>
2022-06-16 14:49:33 +0100

    Improve ListUtil to help with error handling

--- src/main/java/uk/gov/di/ipv/cri/common/library/util/ListUtil.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/util/ListUtil.java
@@ -3,10 +3,13 @@ package uk.gov.di.ipv.cri.common.library.util;
 import java.util.List;
 
 public class ListUtil {
-    public <T> T getOneItemOrThrowError(List<T> list) {
-        if (list.size() == 1) return list.get(0);
-
-        throw new IllegalArgumentException(
-                String.format("Parameter must have exactly one value: %s", list));
+    public <T> T getOneItemOrThrowError(List<T> list) throws IllegalArgumentException {
+        if (list.size() == 0) {
+            throw new IllegalArgumentException("No items found");
+        } else if (list.size() > 1) {
+            throw new IllegalArgumentException("More than one item found");
+        } else {
+            return list.get(0);
+        }
     }
 }
