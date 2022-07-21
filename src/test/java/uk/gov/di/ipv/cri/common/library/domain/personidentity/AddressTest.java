9a9dc5a466a0b05e65ec00081db47bf60b84aa7c
Seamus McShane <seamus.mcshane@digital.cabinet-office.gov.uk>
2022-07-21 11:55:29 +0100

    KBV-771: Change Address getAddressType() to return CURRENT for null validFrom and null validUntil.

--- src/test/java/uk/gov/di/ipv/cri/common/library/domain/personidentity/AddressTest.java
+++ src/test/java/uk/gov/di/ipv/cri/common/library/domain/personidentity/AddressTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 class AddressTest {
     @Test
-    void shouldGetPastAddressType() {
+    void shouldGetPreviousAddressTypeWhenValidUntilIsPastDate() {
         Address testAddress = new Address();
         testAddress.setValidUntil(LocalDate.of(2013, 8, 9));
 
@@ -17,7 +17,32 @@ class AddressTest {
     }
 
     @Test
-    void shouldGetCurrentAddressType() {
+    void shouldGetPreviousAddressTypeWithValidUntilIsToday() {
+        Address testAddress = new Address();
+        testAddress.setValidUntil(LocalDate.now());
+
+        assertEquals(AddressType.PREVIOUS, testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldReturnNullAddressTypeWhenValidUntilIsInFuture() {
+        Address testAddress = new Address();
+        testAddress.setValidUntil(LocalDate.now().plusYears(1));
+
+        assertNull(testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldGetPreviousAddressTypeWithValidUntilAndValidFrom() {
+        Address testAddress = new Address();
+        testAddress.setValidFrom(LocalDate.of(2013, 7, 9));
+        testAddress.setValidUntil(LocalDate.of(2013, 8, 9));
+
+        assertEquals(AddressType.PREVIOUS, testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldGetCurrentAddressTypeWithValidFromInPast() {
         Address testAddress = new Address();
         testAddress.setValidFrom(LocalDate.of(2021, 9, 10));
 
@@ -25,8 +50,51 @@ class AddressTest {
     }
 
     @Test
-    void shouldReturnNullAddressTypeWhenValidFromAndValidUntilNotPresent() {
+    void shouldReturnCurrentAddressTypeWhenValidFromAndValidUntilAreNull() {
         Address testAddress = new Address();
+
+        assertEquals(AddressType.CURRENT, testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldReturnCurrentAddressTypeWhenValidFromIsToday() {
+        Address testAddress = new Address();
+        testAddress.setValidFrom(LocalDate.now());
+
+        assertEquals(AddressType.CURRENT, testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldReturnNullAddressTypeWhenValidFromIsInFuture() {
+        Address testAddress = new Address();
+        testAddress.setValidFrom(LocalDate.now().plusYears(1));
+
+        assertNull(testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldReturnNullAddressTypeWhenValidFromAndValidUntilIsToday() {
+        Address testAddress = new Address();
+        testAddress.setValidFrom(LocalDate.now());
+        testAddress.setValidUntil(LocalDate.now());
+
+        assertNull(testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldReturnNullAddressTypeWhenValidFromAndValidUntilAreInFuture() {
+        Address testAddress = new Address();
+        testAddress.setValidFrom(LocalDate.now().plusYears(1));
+        testAddress.setValidUntil(LocalDate.now().plusYears(2));
+
+        assertNull(testAddress.getAddressType());
+    }
+
+    @Test
+    void shouldReturnNullAddressTypeWhenValidFromAndValidUntilAreReversed() {
+        Address testAddress = new Address();
+        testAddress.setValidFrom(LocalDate.now().minusYears(1));
+        testAddress.setValidUntil(LocalDate.now().minusYears(2));
 
         assertNull(testAddress.getAddressType());
     }
