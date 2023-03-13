22c63dc17f7535e6a80f980493f9fdef306a972f
Suraj Kumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-03-13 12:38:05 +0000

    OJ-1305: Updated EventProbe.java to remove JSON and sensitive information from error logs.

---
+++ src/main/java/uk/gov/di/ipv/cri/common/library/error/GenericException.java
@@ -0,0 +1,10 @@
+package uk.gov.di.ipv.cri.common.library.error;
+
+public class GenericException extends Exception {
+
+    public GenericException() {}
+
+    public GenericException(String message) {
+        super(message);
+    }
+}
--- src/main/java/uk/gov/di/ipv/cri/common/library/util/EventProbe.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/util/EventProbe.java
@@ -8,14 +8,42 @@ import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
 import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
 import software.amazon.lambda.powertools.logging.LoggingUtils;
 import software.amazon.lambda.powertools.metrics.MetricsUtils;
+import uk.gov.di.ipv.cri.common.library.error.GenericException;
 
+import java.text.MessageFormat;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EventProbe {
     private static final String GOVUK_SIGNIN_JOURNEY_ID = "govuk_signin_journey_id";
     private static final Logger LOGGER = LogManager.getLogger();
     private static final MetricsLogger METRICS_LOGGER = MetricsUtils.metricsLogger();
+    private static final String JSON_REGEX = "\\{.*\\}";
+    private static final Pattern PATTERN = Pattern.compile(JSON_REGEX);
+    private static final List<String> SENSITIVE_INFORMATION = List.of(
+            // Shared claims
+            "name",
+            "GivenName",
+            "FamilyName",
+            "birthDate",
+            "address",
+            "buildingNumber",
+            "streetName",
+            "postalCode",
+
+            // Generic capture
+            "dob",
+            "name",
+            "address",
+            "birth",
+            "age",
+            "build"
+    );
+    private static final String JSON_REMOVED_MSG = "[JSON Omitted]";
+    private static final String PARTIAL_EXCEPTION_MSG = "Partial error message (stripped due to containing sensitive information): ";
 
     public EventProbe log(Level level, Throwable throwable) {
         LOGGER.log(level, throwable.getMessage(), throwable);
@@ -35,8 +63,12 @@ public class EventProbe {
     private void logErrorCause(Throwable throwable) {
         Throwable cause = throwable.getCause();
         if (Objects.nonNull(cause)) {
-            LOGGER.error(cause.getMessage(), cause);
-            logErrorCause(cause);
+            String errorMessage = stripJSON(cause.getMessage(), JSON_REMOVED_MSG);
+            int sensitiveInformationStartIdx = containsSensitiveInformation(errorMessage);
+            if(sensitiveInformationStartIdx != -1) {
+                errorMessage = String.format("%s %s", PARTIAL_EXCEPTION_MSG, errorMessage.substring(0, sensitiveInformationStartIdx));
+            }
+            LOGGER.error(errorMessage, new GenericException(errorMessage));
         }
     }
 
@@ -73,5 +105,25 @@ public class EventProbe {
             dimensions.forEach(dimensionSet::addDimension);
             METRICS_LOGGER.putDimensions(dimensionSet);
         }
+    }
+
+    private String stripJSON(String input, String replacement) {
+        String str = input;
+        Matcher matcher = PATTERN.matcher(str);
+        while(matcher.find()) {
+            str = str.replace(matcher.group(), replacement);
+        }
+        return str;
+    }
+
+    private int containsSensitiveInformation(String input) {
+        int index = -1;
+        for(String s : SENSITIVE_INFORMATION) {
+            if(input.toLowerCase().contains(s)) {
+                index = input.toLowerCase().indexOf(s);
+                return index;
+            }
+        }
+        return index;
     }
 }
