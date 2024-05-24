8fa26d79a8f2c6ba527c6dee8c4f532290c3c110
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2024-05-24 12:47:38 +0100

    OJ-2545: Add a method to ListUtil to construct a list excluding elements from another list using a custom comparator

--- src/main/java/uk/gov/di/ipv/cri/common/library/util/ListUtil.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/util/ListUtil.java
@@ -1,8 +1,11 @@
 package uk.gov.di.ipv.cri.common.library.util;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public final class ListUtil {
     private ListUtil() {}
@@ -38,5 +41,38 @@ public final class ListUtil {
         }
 
         return batches;
+    }
+
+    /**
+     * Create a new list comprised of elements in the {@code source} list except those in the {@code
+     * except} when compared using a custom {@code mapper} function.
+     *
+     * @param source The source list to filter
+     * @param except The list containing the elements to exclude
+     * @param mapper A function mapping list elements to values used to determine element equality
+     */
+    public static <T, U extends Comparable<? super U>> List<T> except(
+            List<T> source, List<T> except, Function<? super T, ? extends U> mapper) {
+        return except(source, except, Comparator.comparing(mapper));
+    }
+
+    /**
+     * Create a new list comprised of elements in the {@code source} list except those in the {@code
+     * except} when compared using a custom {@link Comparator comparator} function.
+     *
+     * @see ListUtil#except(List, List, Function)
+     */
+    public static <T> List<T> except(List<T> source, List<T> except, Comparator<T> comparator) {
+        return source.stream()
+                .filter(element -> !contains(except, element, comparator))
+                .collect(Collectors.toList());
+    }
+
+    private static <T> boolean contains(List<T> list, T exclude, Comparator<T> comparator) {
+        return list.stream().anyMatch(element -> objectsEqual(element, exclude, comparator));
+    }
+
+    private static <T> boolean objectsEqual(T a, T b, Comparator<T> comparator) {
+        return comparator.compare(a, b) == 0;
     }
 }
--- src/test/java/uk/gov/di/ipv/cri/common/library/util/ListUtilTest.java
+++ src/test/java/uk/gov/di/ipv/cri/common/library/util/ListUtilTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.awt.Point;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -95,5 +99,129 @@ class ListUtilTest {
         assertEquals(2, batches.size());
         assertEquals(List.of(1, 2, 3), batches.get(0));
         assertEquals(List.of(4, 5), batches.get(1));
+    }
+
+    @Test
+    void except_shouldThrowIfSourceListIsNull() {
+        final List<Object> except = Collections.emptyList();
+
+        assertThrows(
+                NullPointerException.class, () -> ListUtil.except(null, except, Object::toString));
+    }
+
+    @Test
+    void except_shouldThrowIfExcludeListIsNull() {
+        final List<Integer> source = Collections.singletonList(1);
+
+        assertThrows(
+                NullPointerException.class, () -> ListUtil.except(source, null, Object::toString));
+    }
+
+    @Test
+    void except_shouldReturnEmptyListIfSourceListIsEmpty() {
+        assertEquals(
+                Collections.emptyList(),
+                ListUtil.except(
+                        Collections.emptyList(), Collections.emptyList(), Object::toString));
+
+        assertEquals(
+                Collections.emptyList(),
+                ListUtil.except(Collections.emptyList(), List.of(1, 2, 3), Object::toString));
+    }
+
+    @Test
+    void except_shouldReturnEmptyListIfSourceListMatchesExcludeList() {
+        final List<Integer> list = List.of(1, 2, 3);
+        assertEquals(
+                Collections.emptyList(),
+                ListUtil.except(list, List.copyOf(list), Object::toString));
+    }
+
+    @Test
+    void except_shouldReturnSourceListIfExcludeListIsEmpty() {
+        final List<Integer> list = List.of(1, 2, 3);
+        assertEquals(list, ListUtil.except(list, Collections.emptyList(), Object::toString));
+    }
+
+    @Test
+    void except_shouldReturnSourceListIfExcludeListDoesNotHaveCommonElements() {
+        final List<Integer> list = List.of(1, 2, 3);
+        assertEquals(list, ListUtil.except(list, List.of(4, 5, 6), Object::toString));
+    }
+
+    @Test
+    void except_shouldExcludeAllElementsFromExcludeList() {
+        final List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        assertEquals(List.of(1, 3, 6), ListUtil.except(list, List.of(2, 4, 5), Object::toString));
+    }
+
+    @Test
+    void except_shouldExcludeAllElementsContainedInExcludeList() {
+        final List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        assertEquals(
+                List.of(2, 4, 5, 6), ListUtil.except(list, List.of(1, 3, 7, 8), Object::toString));
+    }
+
+    @Test
+    void except_shouldExcludeComplexTypes() {
+        final List<Point> points = List.of(new Point(1, 2), new Point(3, 4));
+
+        assertEquals(
+                List.of(new Point(1, 2)),
+                ListUtil.except(
+                        points, Collections.singletonList(points.get(1)), Object::toString));
+    }
+
+    @Test
+    void except_shouldExcludeCollections() {
+        final List<Collection<Integer>> lists = List.of(List.of(1, 2, 3), List.of(4, 5, 6));
+
+        assertEquals(
+                List.of(List.of(1, 2, 3)),
+                ListUtil.except(lists, Collections.singletonList(lists.get(1)), Object::toString));
+    }
+
+    @Test
+    void except_shouldExcludeMaps() {
+        final List<Map<String, Integer>> maps = List.of(Map.of("one", 1), Map.of("two", 2));
+
+        assertEquals(
+                List.of(Map.of("one", 1)),
+                ListUtil.except(maps, Collections.singletonList(maps.get(1)), Object::toString));
+    }
+
+    @Test
+    void except_shouldExcludeUsingCustomComparator() {
+        final List<Point> points = List.of(new Point(1, 2), new Point(3, 4));
+        final List<Point> exclude = Collections.singletonList(new Point(1, 4));
+
+        assertEquals(
+                List.of(new Point(3, 4)),
+                ListUtil.except(points, exclude, Comparator.comparing(Point::getX)));
+
+        assertEquals(
+                List.of(new Point(1, 2)),
+                ListUtil.except(points, exclude, Comparator.comparing(Point::getY)));
+
+        assertEquals(
+                points,
+                ListUtil.except(
+                        points,
+                        exclude,
+                        Comparator.comparing(Point::getY).thenComparing(Point::getX)));
+    }
+
+    @Test
+    void except_shouldExcludeUsingFieldValue() {
+        final List<Point> points = List.of(new Point(1, 2), new Point(3, 4));
+        final Point exclude = new Point(1, 4);
+
+        assertEquals(
+                List.of(new Point(3, 4)),
+                ListUtil.except(points, Collections.singletonList(exclude), Point::getX));
+
+        assertEquals(
+                List.of(new Point(1, 2)),
+                ListUtil.except(points, Collections.singletonList(exclude), Point::getY));
     }
 }
