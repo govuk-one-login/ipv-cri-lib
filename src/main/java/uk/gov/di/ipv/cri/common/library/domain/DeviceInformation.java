4d6f469c956d6dcb3199c399d383c5d537395504
Suraj Kumar <sk96.uk@gmail.com>
2024-10-31 16:15:40 +0000

    OJ-2739: Fixes to AuditEvent

--- src/main/java/uk/gov/di/ipv/cri/common/library/domain/DeviceInformation.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/domain/DeviceInformation.java
@@ -1,9 +1,11 @@
 package uk.gov.di.ipv.cri.common.library.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DeviceInformation {
     @JsonProperty(value = "encoded")
     private String encoded;
