c17c18dcb4e2b91da1daae223625f3a6198b4aa8
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2024-11-01 17:52:58 +0000

    OJ-2739: Add test harness response to TestContext
    Add a new field since the test harness returns a different response type.
    Add a method to get the test harness response body from CriTestContext.

--- src/testFixtures/java/uk/gov/di/ipv/cri/common/library/stepdefinitions/CriTestContext.java
+++ src/testFixtures/java/uk/gov/di/ipv/cri/common/library/stepdefinitions/CriTestContext.java
@@ -1,19 +1,38 @@
 package uk.gov.di.ipv.cri.common.library.stepdefinitions;
 
+import software.amazon.awssdk.http.HttpExecuteResponse;
+import uk.gov.di.ipv.cri.common.library.client.TestResourcesClient;
+
+import java.io.IOException;
 import java.net.http.HttpResponse;
 
 public class CriTestContext {
+    private HttpExecuteResponse testHarnessResponse;
     private HttpResponse<String> response;
     private String sessionId;
     private String accessToken;
     private String serialisedUserIdentity;
+    private String testHarnessResponseBody;
 
     public HttpResponse<String> getResponse() {
         return response;
     }
 
+    public HttpExecuteResponse getTestHarnessResponse() {
+        return testHarnessResponse;
+    }
+
+    public String getTestHarnessResponseBody() {
+        return testHarnessResponseBody;
+    }
+
     public void setResponse(HttpResponse<String> response) {
         this.response = response;
+    }
+
+    public void setResponse(HttpExecuteResponse response) throws IOException {
+        this.testHarnessResponse = response;
+        this.testHarnessResponseBody = TestResourcesClient.getResponseBody(response);
     }
 
     public String getSessionId() {
