a172a452bc85cc80e0eb652e08298d7722405774
Suraj Kumar <sk96.uk@gmail.com>
2025-04-28 11:35:13 +0100

    feat: create models to represent the jwks endpoint
    refs: OJ-2972

---
+++ src/main/java/uk/gov/di/ipv/cri/common/library/domain/jwks/JWKS.java
@@ -0,0 +1,34 @@
+package uk.gov.di.ipv.cri.common.library.domain.jwks;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JWKS {
+    @JsonProperty("keys")
+    private List<Key> keys;
+
+    private int maxAgeFromCacheControlHeader;
+
+    public JWKS() {
+        // Empty constructor for Jackson
+    }
+
+    public List<Key> getKeys() {
+        return keys;
+    }
+
+    public void setKeys(List<Key> keys) {
+        this.keys = keys;
+    }
+
+    public int getMaxAgeFromCacheControlHeader() {
+        return maxAgeFromCacheControlHeader;
+    }
+
+    public void setMaxAgeFromCacheControlHeader(int maxAgeFromCacheControlHeader) {
+        this.maxAgeFromCacheControlHeader = maxAgeFromCacheControlHeader;
+    }
+}
