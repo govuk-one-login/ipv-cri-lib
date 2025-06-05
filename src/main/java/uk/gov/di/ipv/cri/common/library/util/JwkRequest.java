8160b802c4d0714d8adb9e6c4e2d3ca35d71ea2a
Suraj Kumar <sk96.uk@gmail.com>
2025-06-05 18:26:10 +0100

    spotless
    refs: OJ-3218

--- src/main/java/uk/gov/di/ipv/cri/common/library/util/JwkRequest.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/util/JwkRequest.java
@@ -10,7 +10,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.Optional;
 
 public class JwkRequest {
