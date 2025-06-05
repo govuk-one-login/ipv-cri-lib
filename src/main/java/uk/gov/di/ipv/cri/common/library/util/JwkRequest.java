5c1d91e1b2c74cc9e2f9747fba5e072a3d8c0c57
Suraj Kumar <sk96.uk@gmail.com>
2025-06-05 18:16:56 +0100

    add NOSONAR
    refs: OJ-3218

--- src/main/java/uk/gov/di/ipv/cri/common/library/util/JwkRequest.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/util/JwkRequest.java
@@ -10,6 +10,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Optional;
 
 public class JwkRequest {
