faa569b120dab2702206839c99320d2a137ec282
Suraj Kumar <sk96.uk@gmail.com>
2025-06-09 10:28:34 +0100

    feat: log the original exception name inside JWKSRequestException
    refs: OJ-3218

--- src/main/java/uk/gov/di/ipv/cri/common/library/exception/JWKSRequestException.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/exception/JWKSRequestException.java
@@ -1,11 +1,21 @@
 package uk.gov.di.ipv.cri.common.library.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JWKSRequestException extends Exception {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JWKSRequestException.class);
+
     public JWKSRequestException(String message) {
         super(message);
     }
 
     public JWKSRequestException(String message, Throwable cause) {
         super(message, cause);
+        LOGGER.error(
+                "{} called with cause {}: {}",
+                JWKSRequestException.class.getSimpleName(),
+                cause.getClass().getName(),
+                cause.getMessage());
     }
 }
