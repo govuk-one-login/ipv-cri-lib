f32b6abfa0070086d0409f5952530c6d88763b5f
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2025-09-23 15:20:55 +0100

    OJ-3405: Refactor ConfigurationServiceTest
    Use nesting to better organise the test concerns of Configuration service
    added some missing test cases

--- src/test/java/uk/gov/di/ipv/cri/common/library/service/ConfigurationServiceTest.java
+++ src/test/java/uk/gov/di/ipv/cri/common/library/service/ConfigurationServiceTest.java
@@ -1,6 +1,7 @@
 package uk.gov.di.ipv.cri.common.library.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -13,17 +14,23 @@ import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 import java.time.Clock;
 import java.time.Instant;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.di.ipv.cri.common.library.service.ConfigurationService.SSMParameterName.AUTH_REQUEST_KMS_ENCRYPTION_KEY_ID;
+import static uk.gov.di.ipv.cri.common.library.service.ConfigurationService.SSMParameterName.MAXIMUM_JWT_TTL;
+import static uk.gov.di.ipv.cri.common.library.service.ConfigurationService.SSMParameterName.SESSION_TTL;
+import static uk.gov.di.ipv.cri.common.library.service.ConfigurationService.SSMParameterName.VERIFIABLE_CREDENTIAL_ISSUER;
+import static uk.gov.di.ipv.cri.common.library.service.ConfigurationService.SSMParameterName.VERIFIABLE_CREDENTIAL_SIGNING_KEY_ID;
 
 @ExtendWith({MockitoExtension.class, SystemStubsExtension.class})
 class ConfigurationServiceTest {
-    private static final String TEST_STACK_NAME = "stack-name";
     private static final String PARAM_NAME_FORMAT = "/%s/%s";
     private static final String PARAM_NAME = "param-name";
     private static final String PARAM_VALUE = "param-value";
+
     @Mock private SSMProvider mockSsmProvider;
     @Mock private SecretsProvider mockSecretsProvider;
     @Mock private Clock mockClock;
@@ -31,7 +38,17 @@ class ConfigurationServiceTest {
 
     @SystemStub
     private EnvironmentVariables environment =
-            new EnvironmentVariables("AWS_REGION", "eu-west-2", "AWS_STACK_NAME", "my-stack-name");
+            new EnvironmentVariables(
+                    "AWS_REGION",
+                    "eu-west-2",
+                    "AWS_STACK_NAME",
+                    "my-stack-name",
+                    "SQS_AUDIT_EVENT_QUEUE_URL",
+                    "https://test-audit-queue",
+                    "SQS_AUDIT_EVENT_PREFIX",
+                    "AUDIT-PREFIX");
+
+    private String stackName = environment.getVariables().get("AWS_STACK_NAME");
 
     @BeforeEach
     void setUp() {
@@ -39,97 +56,186 @@ class ConfigurationServiceTest {
                 new ConfigurationService(
                         mockSsmProvider,
                         mockSecretsProvider,
-                        TEST_STACK_NAME,
+                        stackName,
                         null,
-                        TEST_STACK_NAME,
+                        stackName,
                         mockClock);
-    }
 
-    @Test
-    void shouldGetParamValueByName() {
-        String fullParamName = String.format(PARAM_NAME_FORMAT, TEST_STACK_NAME, PARAM_NAME);
+        String fullParamName = String.format(PARAM_NAME_FORMAT, stackName, PARAM_NAME);
         when(mockSsmProvider.get(fullParamName)).thenReturn(PARAM_VALUE);
+
         assertEquals(PARAM_VALUE, configurationService.getParameterValue(PARAM_NAME));
         verify(mockSsmProvider).get(fullParamName);
     }
 
-    @Test
-    void shouldGetSecretValueByNameThatHasTheStackName() {
-        String secretName = "my-secret-name";
-        String secretValue = "secret-value";
-        String fullSecretName = String.format(PARAM_NAME_FORMAT, TEST_STACK_NAME, secretName);
-        when(mockSecretsProvider.get(fullSecretName)).thenReturn(secretValue);
-        assertEquals(secretValue, configurationService.getSecretValue(secretName));
-        verify(mockSecretsProvider).get(fullSecretName);
+    @Nested
+    class ConfigServiceGetSecrets {
+        private static final String SECRET_NAME = "my-secret-name";
+        private static final String SECRET_VALUE = "secret-value";
+
+        @Test
+        void shouldGetSecretValueByNameThatHasTheStackName() {
+            String fullSecretName = String.format(PARAM_NAME_FORMAT, stackName, SECRET_NAME);
+
+            when(mockSecretsProvider.get(fullSecretName)).thenReturn(SECRET_VALUE);
+
+            assertEquals(SECRET_VALUE, configurationService.getSecretValue(SECRET_NAME));
+            verify(mockSecretsProvider).get(fullSecretName);
+        }
+
+        @Test
+        void shouldGetSecretValueByNameThatTheSecretPrefixProvided() {
+            String secretPrefix = "customSecretPrefix";
+            configurationService =
+                    new ConfigurationService(
+                            mockSsmProvider,
+                            mockSecretsProvider,
+                            stackName,
+                            null,
+                            secretPrefix,
+                            mockClock);
+
+            String fullSecretName = String.format(PARAM_NAME_FORMAT, secretPrefix, SECRET_NAME);
+            when(mockSecretsProvider.get(fullSecretName)).thenReturn(SECRET_VALUE);
+
+            assertEquals(SECRET_VALUE, configurationService.getSecretValue(SECRET_NAME));
+            verify(mockSecretsProvider).get(fullSecretName);
+        }
     }
 
-    @Test
-    void shouldGetSecretValueByNameThatTheSecretPrefixProvided() {
-        String secretPrefix = "customSecretPrefix";
-        configurationService =
-                new ConfigurationService(
-                        mockSsmProvider,
-                        mockSecretsProvider,
-                        TEST_STACK_NAME,
-                        null,
-                        secretPrefix,
-                        mockClock);
-        String secretName = "my-secret-name";
-        String secretValue = "secret-value";
-        String fullSecretName = String.format(PARAM_NAME_FORMAT, secretPrefix, secretName);
-        when(mockSecretsProvider.get(fullSecretName)).thenReturn(secretValue);
-        assertEquals(secretValue, configurationService.getSecretValue(secretName));
-        verify(mockSecretsProvider).get(fullSecretName);
+    @Nested
+    class ConfigServiceGetParameters {
+        @Test
+        void shouldGetVerifiableCredentialKmsSigningKeyId() {
+            String vcSigningKmsId = UUID.randomUUID().toString();
+            String fullVcKmsSigningKeyPath =
+                    String.format(
+                            PARAM_NAME_FORMAT,
+                            stackName,
+                            VERIFIABLE_CREDENTIAL_SIGNING_KEY_ID.parameterName);
+            when(mockSsmProvider.get(fullVcKmsSigningKeyPath)).thenReturn(vcSigningKmsId);
+
+            assertEquals(
+                    vcSigningKmsId, configurationService.getVerifiableCredentialKmsSigningKeyId());
+
+            verify(mockSsmProvider).get(fullVcKmsSigningKeyPath);
+        }
+
+        @Test
+        void shouldGetKmsEncryptionKeyId() {
+            String vcKmsEncryptionKeyId = UUID.randomUUID().toString();
+            String fullKmsEncryptionKeyPath =
+                    String.format(
+                            PARAM_NAME_FORMAT,
+                            stackName,
+                            AUTH_REQUEST_KMS_ENCRYPTION_KEY_ID.parameterName);
+            when(mockSsmProvider.get(fullKmsEncryptionKeyPath)).thenReturn(vcKmsEncryptionKeyId);
+
+            assertEquals(vcKmsEncryptionKeyId, configurationService.getKmsEncryptionKeyId());
+
+            verify(mockSsmProvider).get(fullKmsEncryptionKeyPath);
+        }
+
+        @Test
+        void shouldGetMaxJwtTtlTtl() {
+            long maxJwtTtl = 10;
+            when(mockSsmProvider.get(
+                            String.format(
+                                    PARAM_NAME_FORMAT, stackName, MAXIMUM_JWT_TTL.parameterName)))
+                    .thenReturn(String.valueOf(maxJwtTtl));
+
+            assertEquals(maxJwtTtl, configurationService.getMaxJwtTtl());
+        }
+
+        @Test
+        void shouldGetParameterByAbsoluteName() {
+            when(mockSsmProvider.get(PARAM_NAME)).thenReturn(PARAM_VALUE);
+
+            assertEquals(
+                    PARAM_VALUE, configurationService.getParameterValueByAbsoluteName(PARAM_NAME));
+            verify(mockSsmProvider).get(PARAM_NAME);
+        }
     }
 
-    @Test
-    void shouldGetSessionExpirationEpoch() {
-        long sessionTtl = 10;
-        when(mockSsmProvider.get(
-                        String.format(
-                                PARAM_NAME_FORMAT,
-                                TEST_STACK_NAME,
-                                ConfigurationService.SSMParameterName.SESSION_TTL.parameterName)))
-                .thenReturn(String.valueOf(sessionTtl));
-        when(mockClock.instant()).thenReturn(Instant.ofEpochSecond(1655203417));
-        assertEquals(1655203427, configurationService.getSessionExpirationEpoch());
+    @Nested
+    class ConfigServiceGetCommonParameters {
+        private static final String COMMON_PARAM_PREFIX = "common-param-prefix";
+        private String fullPathSessionTtl =
+                String.format(PARAM_NAME_FORMAT, COMMON_PARAM_PREFIX, SESSION_TTL.parameterName);
+
+        @BeforeEach
+        void setUp() {
+            configurationService =
+                    new ConfigurationService(
+                            mockSsmProvider,
+                            mockSecretsProvider,
+                            stackName,
+                            COMMON_PARAM_PREFIX,
+                            stackName,
+                            mockClock);
+
+            String fullPathParams =
+                    String.format(PARAM_NAME_FORMAT, COMMON_PARAM_PREFIX, PARAM_NAME);
+            when(mockSsmProvider.get(fullPathParams)).thenReturn(PARAM_VALUE);
+
+            assertEquals(PARAM_VALUE, configurationService.getCommonParameterValue(PARAM_NAME));
+            verify(mockSsmProvider).get(fullPathParams);
+        }
+
+        @Test
+        void shouldGetSessionExpirationEpoch() {
+            long sessionTtl = 10;
+
+            when(mockSsmProvider.get(fullPathSessionTtl)).thenReturn(String.valueOf(sessionTtl));
+
+            when(mockClock.instant()).thenReturn(Instant.ofEpochSecond(1655203417));
+
+            assertEquals(1655203427, configurationService.getSessionExpirationEpoch());
+            verify(mockSsmProvider).get(fullPathSessionTtl);
+        }
+
+        @Test
+        void shouldGetVerifiableCredentialIssuer() {
+            String vcIssuer = "http://test-vc/issuer";
+
+            String fullPathVcIssuer =
+                    String.format(
+                            PARAM_NAME_FORMAT,
+                            COMMON_PARAM_PREFIX,
+                            VERIFIABLE_CREDENTIAL_ISSUER.parameterName);
+            when(mockSsmProvider.get(fullPathVcIssuer)).thenReturn(vcIssuer);
+
+            assertEquals(vcIssuer, configurationService.getVerifiableCredentialIssuer());
+            verify(mockSsmProvider).get(fullPathVcIssuer);
+        }
+
+        @Test
+        void shouldGetSessionTtl() {
+            long sessionTtl = 10;
+
+            when(mockSsmProvider.get(fullPathSessionTtl)).thenReturn(String.valueOf(sessionTtl));
+
+            assertEquals(sessionTtl, configurationService.getSessionTtl());
+            verify(mockSsmProvider).get(fullPathSessionTtl);
+        }
     }
 
-    @Test
-    void shouldGetSessionTtl() {
-        long sessionTtl = 10;
-        when(mockSsmProvider.get(
-                        String.format(
-                                PARAM_NAME_FORMAT,
-                                TEST_STACK_NAME,
-                                ConfigurationService.SSMParameterName.SESSION_TTL.parameterName)))
-                .thenReturn(String.valueOf(sessionTtl));
-        assertEquals(sessionTtl, configurationService.getSessionTtl());
-    }
+    @Nested
+    class ConfigServiceGetEnvironmentVariables {
+        @Test
+        void shouldGetSqsAuditEventQueueUrlFromEnv() {
 
-    @Test
-    void shouldGetCommonParameterValueUsingCommonPrefix() {
-        String commonParamPrefix = "common-param-prefix";
-        configurationService =
-                new ConfigurationService(
-                        mockSsmProvider,
-                        mockSecretsProvider,
-                        TEST_STACK_NAME,
-                        commonParamPrefix,
-                        TEST_STACK_NAME,
-                        mockClock);
+            String audiEventQueueUrl = configurationService.getSqsAuditEventQueueUrl();
 
-        when(mockSsmProvider.get(String.format(PARAM_NAME_FORMAT, commonParamPrefix, PARAM_NAME)))
-                .thenReturn(PARAM_VALUE);
-        assertEquals(PARAM_VALUE, configurationService.getCommonParameterValue(PARAM_NAME));
-        verify(mockSsmProvider)
-                .get(String.format(PARAM_NAME_FORMAT, commonParamPrefix, PARAM_NAME));
-    }
+            assertEquals("https://test-audit-queue", audiEventQueueUrl);
+        }
 
-    @Test
-    void shouldGetParameterByAbsoluteName() {
-        when(mockSsmProvider.get(PARAM_NAME)).thenReturn(PARAM_VALUE);
-        assertEquals(PARAM_VALUE, configurationService.getParameterValueByAbsoluteName(PARAM_NAME));
-        verify(mockSsmProvider).get(PARAM_NAME);
+        @Test
+        void shouldGetAuditEventPrefixFromEnv() {
+
+            String audiEventQueueUrl = configurationService.getSqsAuditEventPrefix();
+
+            assertEquals("AUDIT-PREFIX", audiEventQueueUrl);
+        }
     }
 }
