73ca5cb6deb5a2bb044a365a3cb2c9802034498d
Suraj Kumar <sk96.uk@gmail.com>
2025-11-04 13:22:31 +0000

    feat: include hashed key id in JWT sent to /token
    refs: OJ-3497

--- src/testFixtures/java/uk/gov/di/ipv/cri/common/library/client/StubClient.java
+++ src/testFixtures/java/uk/gov/di/ipv/cri/common/library/client/StubClient.java
@@ -1,5 +1,7 @@
 package uk.gov.di.ipv.cri.common.library.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.JWSAlgorithm;
@@ -13,11 +15,14 @@ import com.nimbusds.oauth2.sdk.auth.PrivateKeyJWT;
 import uk.gov.di.ipv.cri.common.library.config.CriStubClientEnum;
 import uk.gov.di.ipv.cri.common.library.helpers.SSMHelper;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.UUID;
 
 public class StubClient {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private final ClientConfigurationService clientConfigurationService;
     private static final long HOUR_TIME_LIMIT = 3_600_000;
     private final SSMHelper ssmHelper;
@@ -42,22 +47,54 @@ public class StubClient {
                                 new Date(new Date().getTime() + HOUR_TIME_LIMIT))
                         .build();
 
-        ECDSASigner signer = new ECDSASigner(getEcPrivateKey());
+        String privateSigningKey = getPrivateSigningKey();
+        String publicKeyId = sha256(getKidFromPrivateSigningKey(privateSigningKey));
 
+        ECDSASigner signer = new ECDSASigner(getEcPrivateKey(privateSigningKey));
         JWSHeader jwtHeader =
-                new JWSHeader.Builder(JWSAlgorithm.ES256).type(JOSEObjectType.JWT).build();
+                new JWSHeader.Builder(JWSAlgorithm.ES256)
+                        .type(JOSEObjectType.JWT)
+                        .keyID(publicKeyId)
+                        .build();
         SignedJWT signedJWT = new SignedJWT(jwtHeader, claimsSetValues);
         signedJWT.sign(signer);
 
         return new PrivateKeyJWT(signedJWT);
     }
 
-    private ECKey getEcPrivateKey() throws ParseException {
-        return ECKey.parse(
-                ssmHelper.getParameterValue(
-                        String.format(
-                                "/%s/%s/privateSigningKey",
-                                this.clientConfigurationService.getTestResourcesStackName(),
-                                this.clientConfigurationService.getDefaultClientId())));
+    private ECKey getEcPrivateKey(String privateSigningKey) throws ParseException {
+        return ECKey.parse(privateSigningKey);
+    }
+
+    private String getPrivateSigningKey() {
+        return ssmHelper.getParameterValue(
+                String.format(
+                        "/%s/%s/privateSigningKey",
+                        this.clientConfigurationService.getTestResourcesStackName(),
+                        this.clientConfigurationService.getDefaultClientId()));
+    }
+
+    private String getKidFromPrivateSigningKey(String privateSigningKey) {
+        try {
+            return OBJECT_MAPPER.readTree(privateSigningKey).get("kid").asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String sha256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
