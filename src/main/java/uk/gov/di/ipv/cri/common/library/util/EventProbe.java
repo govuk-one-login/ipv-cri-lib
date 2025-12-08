547ef922dd96c9a2c2c8f8af3849791ce167ba86
Suraj Kumar <sk96.uk@gmail.com>
2025-12-08 11:06:38 +0000

    feat: create EventProbe#flush

--- src/main/java/uk/gov/di/ipv/cri/common/library/util/EventProbe.java
+++ src/main/java/uk/gov/di/ipv/cri/common/library/util/EventProbe.java
@@ -26,6 +26,14 @@ public class EventProbe {
         this.metrics = metrics;
     }
 
+    public void flush() {
+        try {
+            metrics.flush();
+        } catch (Exception e) {
+            LOGGER.error("Failed to flush metrics", e);
+        }
+    }
+
     public EventProbe log(Level level, Throwable throwable) {
         LOGGER.log(level, throwable.getMessage(), throwable);
         if (level == Level.ERROR) {
